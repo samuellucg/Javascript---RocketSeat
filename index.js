1d60af253fce0b33421260ae424edbd72ca9f1f7
Samuel Lucas <samuel.lucasmacedos@gmail.com>
2024-09-10 21:44:35 -0300

    primeiros_passos no js

---
+++ index.js
@@ -0,0 +1,34 @@
+// hello world
+console.log("hahahahaha");
+
+// armazenar variavel
+let nome = "Samuel"
+console.log(nome);
+
+// mudar variavel já criada
+nome = "Teresa"
+console.log(nome);
+
+// variavel constante (que não pode ser mudada depois)
+const numero = 1
+console.log(numero)
+
+//arrays
+let arrayzin = ["samuca","brasil",25]
+console.log(arrayzin[2] + " " + arrayzin[0])
+
+//objetos
+let objeto_1 = {
+    value: 'ler um livro por mês!',
+    checked: false,
+    pior: "sim"
+}
+console.log(objeto_1.pior) // você pode dar .alguma coisa q está no objeto como value,checked e etc.
+
+// arrow function
+
+const criarMeta = (parametros) => {
+    console.log(parametros)
+}
+
+// function
